fce75d51c76936b09d4740359f8c509c94056377
Hosung <an3735297@pusan.ac.kr>
2023-05-14 15:23:46 +0900

    feat : test / save member using jpa data

--- jpa-data/src/test/java/study/jpadata/repository/MemberRepositoryTest.java
+++ jpa-data/src/test/java/study/jpadata/repository/MemberRepositoryTest.java
@@ -1,26 +1,27 @@
 package study.jpadata.repository;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.jpadata.entity.Member;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-class MemberJpaRepositoryTest {
+@Rollback(false)
+class MemberRepositoryTest {
     @Autowired
-    MemberJpaRepository memberJpaRepository;
+    MemberRepository memberRepository;
 
     @Test
     public void testMember() {
         Member member = new Member("memberA");
-        Member savedMember = memberJpaRepository.save(member);
-        Member findMember = memberJpaRepository.find(savedMember.getId());
+        Member savedMember = memberRepository.save(member);
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
 
         assertThat(findMember).isEqualTo(savedMember);
     }
