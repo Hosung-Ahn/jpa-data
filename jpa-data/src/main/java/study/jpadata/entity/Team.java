7b80c7ce169045059e3a1766175944ff13d4d80a
Hosung <an3735297@pusan.ac.kr>
2023-05-16 10:14:20 +0900

    fix : team entity
    member 와 마찬가지로 롬복 추가

--- jpa-data/src/main/java/study/jpadata/entity/Team.java
+++ jpa-data/src/main/java/study/jpadata/entity/Team.java
@@ -1,10 +1,18 @@
 package study.jpadata.entity;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
 public class Team {
     @Id @GeneratedValue
     @Column(name = "team_id")
