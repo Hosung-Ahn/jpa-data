f29acde9db190661c622b363d6b8299e7bc5e8e6
Hosung <an3735297@pusan.ac.kr>
2023-05-16 09:54:47 +0900

    feat : Team entity
    team 엔티티를 만들고 member 과 연관관계 매핑을 함
    member : team 은 1:n 관계이고 양방향 매핑이다.

--- jpa-data/src/main/java/study/jpadata/entity/Member.java
+++ jpa-data/src/main/java/study/jpadata/entity/Member.java
@@ -3,16 +3,21 @@ package study.jpadata.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 public class Member {
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String name;
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
 
     protected Member() {
     }
---
+++ jpa-data/src/main/java/study/jpadata/entity/Team.java
@@ -0,0 +1,15 @@
+package study.jpadata.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+    @OneToMany
+    private List<Member> members = new ArrayList<>();
+}
