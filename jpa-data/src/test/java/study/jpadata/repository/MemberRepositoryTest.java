fbd49c91030513f485fd7a94eacce81d16d9234a
Hosung <an3735297@pusan.ac.kr>
2023-05-17 15:46:19 +0900

    feat : test / findByUsernameAndAgeGreaterThan using jpa data

--- jpa-data/src/test/java/study/jpadata/repository/MemberRepositoryTest.java
+++ jpa-data/src/test/java/study/jpadata/repository/MemberRepositoryTest.java
@@ -52,4 +52,18 @@ class MemberRepositoryTest {
         assertThat(memberRepository.count()).isEqualTo(0);
 
     }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThan() {
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("AAA", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findByNameAndAgeGreaterThan("AAA", 15);
+
+        assertThat(result.get(0).getName()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+    }
 }
