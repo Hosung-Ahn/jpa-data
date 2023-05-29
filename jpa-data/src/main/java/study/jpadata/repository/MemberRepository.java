98220262bf27edbdb130794ad8b5478e77c0508d
Hosung <an3735297@pusan.ac.kr>
2023-05-29 16:44:23 +0900

    feat : repository / find user / by query annotation

--- jpa-data/src/main/java/study/jpadata/repository/MemberRepository.java
+++ jpa-data/src/main/java/study/jpadata/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package study.jpadata.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import study.jpadata.entity.Member;
 
@@ -9,4 +11,6 @@ import java.util.List;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByNameAndAgeGreaterThan(String name, int age);
+    @Query("select m from Member m where m.name = :name and m.age > :age")
+    List<Member> findUser(@Param("name") String name, @Param("age") int age);
 }
